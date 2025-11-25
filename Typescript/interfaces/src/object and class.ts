7a2879be14cdf5bd9af431b9bd340032dcfc9f01
Shivam Yadav <shivamy121314@gmail.com>
2025-11-25 10:27:31 +0530

    class with interfaces

--- Typescript/interfaces/src/object and class.ts
+++ Typescript/interfaces/src/object and class.ts
@@ -1,15 +1,55 @@
+
+// interface with function
+
 interface User{
     name : string,
     age : number,
-    greet : () => void
+    greet : () => string,
 }
 
 let user : User = {
     name : "shivam",
     age : 12,
     greet(){
-        console.log("hii")
+        return "hii" + this.name
     }
 }
 
 user.greet();
+
+// interface with classes ( we cannot access the variable like this.name in object) in mine it is working
+
+interface People {
+    name : string,
+    age : number,
+    greet : () => boolean
+}
+
+class Manager implements People {
+    // need to re declare the type with variable (more then given in interface => gender)
+    name: string; 
+    age: number;
+    gender : string;
+
+    constructor(a:string,b:number,c : string){
+        this.age = b;
+        this.name = a;
+        this.gender = c;
+    }
+
+    // function creation
+    greet(){
+        return this.age < 12;
+    }
+}
+
+// can be used like this
+// class God extends Manager {
+//     constructor(name:string,age:number){
+//         super(name,age); // this is used to call the constructor of the class from where it is extended (parent class) so that 
+//         // the value get initalize
+//     }
+// }
+
+let userOffice = new Manager("shivam" , 2 , "yadav");
+userOffice.age // can access element
